1be6c682957fb0c0859fa751cd2e5a8d9f38571e
adarshswamianth <adarshswamianth7@gmail.com>
2023-09-28 23:35:30 +0530

    Bug: make the design better

--- src/Components/Conclusion.jsx
+++ src/Components/Conclusion.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 function Conclusion() {
   return (
-    <div>
-        Conclusion
+    <div className="p-3 mt-6">
+      <h3 className="text-center text-xl font-bold">Conclusion</h3>
+     
     </div>
   );
 }

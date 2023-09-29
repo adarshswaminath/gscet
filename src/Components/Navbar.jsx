1e6d51018c52dfb586d5bddd0eab056df275ea41
adarshswamianth <adarshswamianth7@gmail.com>
2023-09-29 08:40:30 +0530

    restrure: navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -21,7 +21,7 @@ function Navbar() {
         });
       }, []);
   return (
-    <div className='z-50 p-2 flex items-center justify-between top-0 right-0 sticky bg-white/40 backdrop-blur'>
+    <div className='z-50 p-3 flex items-center justify-between top-0 right-0 sticky bg-white/40 backdrop-blur'>
         <h1 className='text-xl font-bold bg-clip-text text-transparent bg-gradient-to-tr from-blue-400 to-indigo-800'>CodeIgnite.</h1>
         <div className="flex items-center space-x-5 font-medium">
             <a href="#about" className='text-blue-500 smooth-scroll-link'>About</a>
